656c46626cdbc94e934ded3716e04b0dbf67a4aa
MiguelVillamil <>
2021-04-22 23:22:25 +0000

    se completo reto7 sm

---
+++ py2_retos1/reto7.py
@@ -0,0 +1,27 @@
+from termcolor import colored
+def menu():
+  print(colored("Deseas contar de 1 hasta 10? \n","yellow"))
+  print(colored("1. si\n","green"))
+  print(colored("2. no\n","red"))
+  respuesta= input("")
+  return respuesta
+
+def cuenta():
+  for i in range (1,11):
+    print(colored(i,"cyan"))
+
+def main():
+  salida=False
+  while (salida==False):
+    opcion=menu()
+    if (opcion=="1"):
+      cuenta()
+    else:
+      if(opcion=="2"):
+        salida=True
+        print(colored("Gracias por usar el programa","red") )
+      else:   
+        print(colored("No ha ingresado una opcion correcta","red") )
+        continuar()         
+
+main()
