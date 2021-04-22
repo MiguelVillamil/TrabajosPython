eb6f4571708950ba753a57e90bc2010e2818506d
MiguelVillamil <>
2021-04-22 23:25:10 +0000

    se completo reto 8 sm

---
+++ py2_retos1/reto8.py
@@ -0,0 +1,27 @@
+from termcolor import colored
+def menu():
+  print(colored("Deseas contar los pares de 10 hasta 20? \n","yellow"))
+  print(colored("1. si\n","green"))
+  print(colored("2. no\n","red"))
+  respuesta= input("")
+  return respuesta
+
+def cuenta():
+  for i in range (10,21,2):
+    print(colored(i,"cyan"))
+
+def main():
+  salida=False
+  while (salida==False):
+    opcion=menu()
+    if (opcion=="1"):
+      cuenta()
+    else:
+      if(opcion=="2"):
+        salida=True
+        print(colored("Gracias por usar el programa","red") )
+      else:   
+        print(colored("No ha ingresado una opcion correcta","red") )
+        continuar()         
+
+main()
